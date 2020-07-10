40565081acb81dedaad6ca41ea5f5d4349bb6a4a
alvaropneves <alvaropn94@gmail.com>
2020-07-10 17:26:37 -0300

    First commit

---
+++ components/header.js
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+
+const Header = () => {
+  return (
+    <header>
+      <div className="logo">
+        <Link href="/">
+          <a>Tudo sobre fundos de investimento</a>
+        </Link>
+      </div>
+      <style jsx>{`
+        header {
+          text-align: center;
+          margin: 40px 0;
+        }
+        .logo a {
+          text-shadow: rgba(0, 0, 0, 0.13) 3px 3px 5px;
+          font-size: 40px;
+          font-weight: 900;
+          color: #190;
+        }
+      `}</style>
+    </header>
+  );
+};
+
+export default Header;
