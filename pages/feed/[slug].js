1e66e5c06e3b24d687770dc8ec080f50dbc2df36
alvaropneves <alvaropn94@gmail.com>
2020-08-11 21:21:21 -0300

    test com getinitialprops

--- pages/feed/[slug].js
+++ pages/feed/[slug].js
@@ -12,7 +12,7 @@ export default function Feed({ feed }) {
     <Page title={feed.title}>
       <div className="feed">
         <img className="image" src={`/static/images/${feed.slug}.png`} />
-        <h1>{feed.title} as</h1>
+        <h1>{feed.title}</h1>
         <div className="links">
           <a href={feed.website} target="_blank">
             Website
@@ -25,7 +25,7 @@ export default function Feed({ feed }) {
         risco: {feed.risk}
       </div>
 
-    
+      {feed.posts.length ? <PostList posts={feed.posts} /> : <div>Posts are not available at the moment</div>}
       <style jsx>{`
         .feed {
           margin-bottom: 30px;
@@ -48,5 +48,16 @@ export default function Feed({ feed }) {
   );
 }
 
+Feed.getInitialProps = async ({ res, query }) => {
+  let api_base = process.env.NODE_ENV == 'production' ? 'https://next-feed-reader.now.sh' : 'http://localhost:3000';
+  let r = await fetch(`${api_base}/api/feeds/${query.slug}`);
+  let feed = await r.json();
 
+  if (feed.error && res) {
+    res.statusCode = 404;
+  }
 
+  return {
+    feed
+  };
+};
